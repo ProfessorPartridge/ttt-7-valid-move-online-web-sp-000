a7b09ddab8a329760d678ed1d6edb975f86b4e3f
Josiah  Partridge <jpartridj94@gmail.com>
2020-01-28 03:42:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?(board, index) == true || index
+  if position_taken?(board, index) == true || index.between(0 , 8)
     puts "That position is already populated with a value. Please try again"
   else
     true
